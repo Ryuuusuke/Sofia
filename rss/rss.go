e1c6f7a28a51dc57b5fbf54e59781f7a3ec06271
Ryuuusuke <makewishmr@gmail.com>
2025-07-06 19:53:55 +0700

    Feat: Experimental feature supporting RDF version of RSS

--- rss/rss.go
+++ rss/rss.go
@@ -22,9 +22,17 @@ type Channel struct {
 }
 
 type Item struct {
-	Title   string `xml:"title"`
-	Link    string `xml:"link"`
-	PubDate string `xml:"PubDate"`
+	Title string `xml:"title"`
+	Link  string `xml:"link"`
+}
+
+type RDFChannel struct {
+	Title string `xml:"title"`
+}
+
+type RDF struct {
+	Items   []Item     `xml:"item"`
+	Channel RDFChannel `xml:"channel"`
 }
 
 func FetchRSS(feedURL string) ([]Item, string, error) {
@@ -35,12 +43,24 @@ func FetchRSS(feedURL string) ([]Item, string, error) {
 	defer resp.Body.Close()
 
 	var rss RSS
-	err = xml.NewDecoder(resp.Body).Decode(&rss)
+	if err := xml.NewDecoder(resp.Body).Decode(&rss); err == nil && len(rss.Channel.Items) > 0 {
+		return rss.Channel.Items, rss.Channel.Title, nil
+	}
+
+	resp.Body.Close()
+	resp, err = http.Get(feedURL)
 	if err != nil {
 		return nil, "", err
 	}
+	defer resp.Body.Close()
 
-	return rss.Channel.Items, rss.Channel.Title, nil
+	var rdf RDF
+	if err := xml.NewDecoder(resp.Body).Decode(&rdf); err == nil && len(rdf.Items) > 0 {
+		source := rdf.Channel.Title
+		return rdf.Items, source, nil
+	}
+
+	return nil, "", fmt.Errorf("unable to parse items from feed: %s", feedURL)
 }
 
 func removeQueryParams(rawUrl string) string {
